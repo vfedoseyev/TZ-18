a886b8567b133bb41ce425bdca8eee73e3ecf1a3
mr.fedoseyev <mr.fedoseyev@mail.ru>
2021-06-13 17:03:38 +0300

    add homework

---
+++ spread.js
@@ -0,0 +1,92 @@
+const stuff = [
+    [
+        '♈',
+        '♉',
+        '♊',
+        '♋',
+        '♌',
+    ],
+    [
+        '♍',
+        '♎',
+    ],
+    [
+        [
+            '♏',
+            '♐',
+            '♑',
+        ],
+        [
+            '♒',
+            '♓',
+        ],
+        [
+            '🌸',
+            '🌷',
+            '🌹',
+            '🌺',
+        ]
+    ],
+    [
+        '🌻',
+        '🌼',
+        '🌽',
+    ],
+    [
+        [{
+                value: '🍅'
+            },
+            {
+                value: '🍎'
+            },
+        ],
+        [{
+                value: '🍏'
+            },
+            {
+                get: () => '🍑'
+            },
+        ],
+        [{
+                get: () => '🍒'
+            },
+            {
+                get: () => '🍓'
+            },
+        ],
+    ]
+];
+
+/**
+ * Из представленного массива stuff необходимо заполнить
+ * константы zodiacSigns, flowers, food соответственно
+ * знаками задиака, цветами и съедобными объектами.
+ * Значения должны получиться плоскими массивами
+ * без оберток в виде объектов и методов вида:
+ * 🍅 🍎 🍏 🍑 🍒 🍓
+ * Попробуйте написать как можно меньше кода для достижения
+ * результата.
+ */
+
+
+const zodiacSignsFirst = stuff[0];
+const zodiacSignsSecond = stuff[1];
+const zodiacSignsThird = stuff[2][0];
+const zodiacSignsFourth = stuff[2][1];
+const zodiacSigns = [...zodiacSignsFirst, ...zodiacSignsSecond, ...zodiacSignsThird, ...zodiacSignsFourth];
+// const zodiacSigns = [...stuff[0],...stuff[1],...stuff[2][0],...stuff[2][1]];
+
+const flowersFirst= stuff[2][2];
+const flowersSecond= stuff[3];
+const flowers = [...flowersFirst, ...flowersSecond.slice(0, -1)];
+// const flowers = [...stuff[2][2], ...stuff[3].slice(0, -1)];
+
+const foodZero = stuff[3]
+const firstFood = [...stuff][4][0];
+const secondFood = [...stuff][4][1];
+const thirdFood = [...stuff][4][2];
+
+const food = [...foodZero.slice(2, ), firstFood[0].value, firstFood[1].value, secondFood[0].value, secondFood[1].get(), thirdFood[0].get(), thirdFood[1].get()]
+console.log(zodiacSigns);
+console.log(flowers);
+console.log(food);
